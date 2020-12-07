9fe965e4157575ea1c264f76ef7231c2dbef0f3d
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-12-07 18:04:30 -0500

    Add validations

--- api/calculate/calculate.controller.js
+++ api/calculate/calculate.controller.js
@@ -5,10 +5,11 @@
 
 function calculate(req, res) {
   const { name, notas } = req.body
+  const validate = notas.some(nota =>  typeof nota.value !== 'number' || nota.value > 5)
   const sumPercentages = notas.reduce((acc, nota) => acc + nota.percentage, 0)
-  if (sumPercentages > 100) {
-    return res.send({ error: true, message: "La suma de porcentajes debe ser menor a 100" })
-  }
+  if (validate) return res.send({ error: true, message: "El valor de cada nota no puede ser mayor a 5 y debe ser un tipo número" })
+  if (sumPercentages > 100) return res.send({ error: true, message: "La suma de porcentajes debe ser menor a 100" })
+
   const definitiva = notas.reduce((acc, nota) => acc + (nota.value * (nota.percentage / 100)), 0)
   return res.send({ error: false, message: `La nota definitiva de la materia ${name} es: ${definitiva}` })
 }
